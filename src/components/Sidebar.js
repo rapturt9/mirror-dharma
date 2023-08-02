b5bf92b66677f72edf3748d71c3ba876d55e2eee
Ram Potham <ram.potham@gmail.com>
2023-08-02 12:31:22 -0400

    remove articles header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 const Sidebar = ({ entries }) => (
 	<aside className="w-30 pl-4 mr-2">
-		<h2 className="text-2xl font-bold mb-4">Articles</h2>
 		<ul className="space-y-4">
 			{entries.map(entry => (
 				<li key={entry.digest}>
